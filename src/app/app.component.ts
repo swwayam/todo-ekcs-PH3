4e5469c5305e88c21dbfc372fdb8d4631ee84885
ekcs-swayam <ssuryavanshi@mediaferry.com>
2023-12-12 21:47:21 +0530

    TODO - UI Setup and Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,23 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo';
+  // We will use a vaibale to store the userInput and show it on screen
+  todoItem!: any;
+
+  // This function takes input from user
+  getUserInput(todoText: any) {
+    if (todoText.value != '') {
+      console.log(todoText.value);
+      this.todoItem = todoText.value;
+      todoText.value = '';
+    } else {
+      alert('Value cannot be empty');
+    }
+
+    // We used return false to avoid page refresh
+    return false;
+  }
 }
